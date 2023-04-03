5cb9720056a890157a538c9fc3789b98d5d30f01
Huy <trannhathuy1305@gmail.com>
2023-04-03 18:24:41 +0700

    Add search box can find robot we want

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
-import CardList from './CardList'
 import 'tachyons'
-import { robots } from './robots'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
  <React.StrictMode>
-  <div>
-   <CardList robots={robots} />
-  </div>
+  <App />
  </React.StrictMode>
 )
 
